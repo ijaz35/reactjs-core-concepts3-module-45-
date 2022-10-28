5acccc8bda9ed2d4e37e450c1d10bde178758f5e
ijaz35 <ijaz.hossain1@gmail.com>
2022-10-28 15:39:45 +0600

    module-45 practice-3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+const commentStyle = {
+  border: '3px solid blue',
+  borderRadius: '15px',
+  margin: '10px',
+  backgroundColor: 'skyblue'
+
+
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Comments></Comments>
+    </div>
+  );
+}
+function Comments() {
+  const [comments, setComments] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/comments')
+      .then(res => res.json())
+      .then(data => setComments(data))
+  }, []);
+
+  return (
+    <div>
+      <h1>User Comments</h1>
+      <h3>comments: {comments.length}</h3>
+      {
+        comments.map(comment => <Comment name={comment.name} email={comment.email} body={comment.body}></Comment>)
+      }
+    </div>
+  );
+}
+function Comment(props) {
+  return (
+    <div style={commentStyle}>
+      <h2>Name: {props.name}</h2>
+      <h3>Email: {props.email}</h3>
+      <p>{props.body}</p>
     </div>
   );
 }
